90bca598ed32b398d7ad5306b0618ca63fcc26a1
Berat İzgördü <berat@sighup.io>
2018-10-07 15:35:00 +0200

    adding sub commands

--- main.go
+++ main.go
@@ -1,33 +1,21 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"github.com/spf13/viper"
-)
+import "git.incubator.sh/sighup/furyctl/cmd"
 
 func main() {
-	viper.SetConfigType("yml")
-	viper.AddConfigPath(".")
-	viper.SetConfigName(configFile)
-	configuration := new(Furyconf)
-	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalf("Error reading config file, %s", err)
-	}
-	err := viper.Unmarshal(configuration)
-	if err != nil {
-		log.Fatalf("unable to decode into struct, %v", err)
-	}
-	fmt.Println(configuration)
-
-	err = configuration.Validate()
-	if err != nil {
-		log.Println("ERROR VALIDATING: ", err)
-	}
-
-	err = configuration.Download()
-	if err != nil {
-		log.Println("ERROR DOWNLOADING: ", err)
-	}
+	cmd.Execute()
 }
