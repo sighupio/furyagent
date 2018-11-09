19cd6419e15268ab63a6dddf684115d3b8a9fce2
Philippe scorsolini <pscorso93@gmail.com>
2018-11-09 10:38:10 +0100

    test.sh not passing, should read foo=v1 after restore, instead cannot find foo key at  all

--- Makefile
+++ Makefile
@@ -5,12 +5,13 @@ build:
 	mv furyctl bin
 
 copy-and-test-furyctl:
+	sudo docker-compose  up -d 
 	sudo docker cp bin/furyctl furyctl_etcd_1:/bin
 	sudo docker cp furyagent.yml furyctl_etcd_1:/
 	sudo docker cp test.sh furyctl_etcd_1:/
 	sudo docker exec -ti furyctl_etcd_1 sh -c "mkdir -p /etc/etcd/pki && touch /etc/etcd/pki/ca.crt /etc/etcd/pki/ca.key"
 	sudo docker exec -ti furyctl_etcd_1 sh -c "chmod u+x test.sh && ./test.sh"
-	sudo docker restart furyctl_etcd_1
+	sudo docker exec -ti furyctl_etcd_1 usr/local/bin/etcd --name s1 --data-dir /etcd-data --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://0.0.0.0:2379 --listen-peer-urls http://0.0.0.0:2380 --initial-advertise-peer-urls http://0.0.0.0:2380 --initial-cluster s1=http://0.0.0.0:2380 --initial-cluster-token tkn --initial-cluster-state new 1>/dev/null
 	sudo docker exec -ti furyctl_etcd_1 sh -c "echo 'read after restart' && etcdctl get foo"
 	#sudo docker-compose logs -f
 
--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
+/usr/local/bin/etcd --name s1 --data-dir /etcd-data --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://0.0.0.0:2379 --listen-peer-urls http://0.0.0.0:2380 --initial-advertise-peer-urls http://0.0.0.0:2380 --initial-cluster s1=http://0.0.0.0:2380 --initial-cluster-token tkn --initial-cluster-state new 2> /dev/null &
+sleep 10
+
 echo 1 > /etc/etcd/pki/ca.crt
 echo 1 > /etc/etcd/pki/ca.key
 echo "ci sono" > /etcd-data/member/prova
@@ -22,11 +25,14 @@ cat /etc/etcd/pki/ca.crt
 cat /etc/etcd/pki/ca.key
 etcdctl get foo
 cat /etcd-data/member/prova
+killall -KILL etcd
+sleep 10
 
 furyctl restore etcd
 
 echo "read after restoring: "
 cat /etc/etcd/pki/ca.crt
 cat /etc/etcd/pki/ca.key
-cat /etcd-data/member/prova
+/usr/local/bin/etcd --name s1 --data-dir /etcd-data --listen-client-urls http://0.0.0.0:2379 --advertise-client-urls http://0.0.0.0:2379 --listen-peer-urls http://0.0.0.0:2380 --initial-advertise-peer-urls http://0.0.0.0:2380 --initial-cluster s1=http://0.0.0.0:2380 --initial-cluster-token tkn --initial-cluster-state new 2> /dev/null &
+sleep 10
 etcdctl get foo
