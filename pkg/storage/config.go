e5fc3ec77614d062723ac128a1730256e37356fe
Ángel Barrera Sánchez <angel@sighup.io>
2020-03-02 12:29:20 +0100

    Remove S3 struct dead code

--- pkg/storage/config.go
+++ pkg/storage/config.go
@@ -14,12 +14,3 @@ type Config struct {
 	BucketName           string `mapstructure:"bucketName"`
 	LocalPath            string `mapstructure:"path"`
 }
-
-// S3 is the configuration needed to save/open file from s3
-type S3 struct {
-	AccessKey  string `mapstructure:"aws_access_key"`
-	SecretKey  string `mapstructure:"aws_secret_key"`
-	URL        string `mapstructure:"url"`
-	Region     string `mapstructure:"region"`
-	BucketName string `mapstructure:"bucketName"`
-}
