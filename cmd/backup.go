0d597a1faf824f5c2c9a7575c43167ba43f3cadc
Ángel Barrera <angel@sighup.io>
2020-03-02 16:33:02 +0100

    Remove dead code
    * Remove etcd dead code
    * Remove dummy backup/restore master

--- cmd/backup.go
+++ cmd/backup.go
@@ -29,18 +29,7 @@ var etcdBackupCmd = &cobra.Command{
 	},
 }
 
-// masterBackupCmd represents the `furyctl backup master` command
-var masterBackupCmd = &cobra.Command{
-	Use:   "master",
-	Short: "Backups master node",
-	Long:  `Backups master node`,
-	Run: func(cmd *cobra.Command, args []string) {
-
-	},
-}
-
 func init() {
 	rootCmd.AddCommand(backupCmd)
 	backupCmd.AddCommand(etcdBackupCmd)
-	backupCmd.AddCommand(masterBackupCmd)
 }
